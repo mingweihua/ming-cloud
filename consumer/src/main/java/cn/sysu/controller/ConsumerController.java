974771fd2cd8c09cf6b342a9bfa1fbe051bb11c7
ming <984396692@qq.com>
2021-12-11 15:49:01 +0800

    20211211

--- consumer/src/main/java/cn/sysu/controller/ConsumerController.java
+++ consumer/src/main/java/cn/sysu/controller/ConsumerController.java
@@ -2,7 +2,10 @@ package cn.sysu.controller;
 
 import cn.sysu.pojo.ZK_collect;
 import lombok.extern.slf4j.Slf4j;
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,6 +19,9 @@ public class ConsumerController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @RequestMapping("/test")
     public List<ZK_collect> test() {
         log.info("测试方法则行");
@@ -24,5 +30,18 @@ public class ConsumerController {
         return zk_collects;
     }
 
+    /*
+        使用DiscoveryClient，动态从Eureka服务器根据application id/name获取服务
+     */
 
+    @RequestMapping("/test2")
+    public List<ZK_collect> test2() {
+        log.info("动态从Eureka服务器根据application id/name获取服务");
+        List<ServiceInstance> instances = discoveryClient.getInstances("service-ming");
+        ServiceInstance serviceInstance = instances.get(0);
+        String url = serviceInstance.getUri() + "/service/getAllZKData";
+        log.info(url);
+        List<ZK_collect> zk_collects = restTemplate.getForObject(url, List.class);
+        return zk_collects;
+    }
 }
